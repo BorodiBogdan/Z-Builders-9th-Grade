4138d4edafe83f614ba2986b2a0e4e8478dfbc10
Borodi Bogdan <77606868+BorodiBogdan@users.noreply.github.com>
2023-10-29 12:13:10 +0200

    Create for.cpp

---
+++ for.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    int n, k;
+
+    for(int i = 0; i <= 10; i = i + 2)///in  partea din stanga se declara variabilele, // in mijloc se pune conditia// in partea dreapta pot sa schimb variabilele
+        cout << i << " ";
+
+
+    int i = 0;
+
+    for( ;i <= 10; ){
+        i += 2;
+        cout << i << " ";
+    }
+
+    return 0;
+}
