aab244097450d9b70d2083ee35707123b55b4739
Borodi Bogdan <77606868+BorodiBogdan@users.noreply.github.com>
2023-10-29 12:23:22 +0200

    Create citirepanala0.cpp

---
+++ citirepanala0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main(){
+   ///5 9 7 8 19 12 ..... 30 0
+
+   int x;
+
+   cin >> x;
+
+   while(x != 0){///citirea pana la aparitia lui 0
+        cin >> x;
+   }
+
+
+    return 0;
+}
