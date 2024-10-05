120a8fe8a724b9927571139588965a7b7de3f9bf
CalebSig <Sighiartau.Do.Achim@student.utcluj.ro>
2024-10-05 14:28:36 +0300

    Add files via upload

---
+++ phi_indicatorulLuiEuler.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int pow(int n, int m)
+{
+    int p=1;
+    while(m>0) p=p*n;
+    return p;
+}
+
+int main()
+{
+    int n,d=2,cn,p=1;
+    cin>>n;
+    cn=n;
+   /* while(cn>1)
+    {
+        if(cn%d==0)
+        {
+            while(cn%d==0) cn=cn/d;
+            n=n/d*(d-1); //cu verde
+        }
+        d++;
+        if(d*d>cn && cn>1) d=cn;
+    }*/
+
+    while(n>1)
+    {
+        if(n%d==0)
+        {
+            int e=0;
+            while(cn%d==0) {cn=cn/d; e++;}
+            p=p*pow(d,e-1)*(d-1); ///cu mov
+        }
+        d++;
+        if(d*d>n && n>1) d=n;
+    }
+
+    cout<<n;
+    return 0;
+}
