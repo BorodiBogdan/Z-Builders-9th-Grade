258c8ab6e220a0568519d41818ff2073cc2cb795
CalebSig <Sighiartau.Do.Achim@student.utcluj.ro>
2025-02-01 14:09:40 +0200

    Update ListeSimpluInlantuite.cpp

--- ListeSimpluInlantuite.cpp
+++ ListeSimpluInlantuite.cpp
@@ -107,15 +107,70 @@ void afisList(nod *head)
     }
 }
 
+nod *stergereDinFata(nod *head)
+{
+    if(head!=NULL)
+    {
+        nod *p=head;
+        head=head->next;
+        delete(p);
+    }
+    return head;
+}
+
+nod *stergereCoada(nod *head)
+{
+    if(head==NULL) return head;
+    if(head->next!=NULL)
+    {
+        nod *p=head;
+        while(p->next->next!=NULL) p=p->next;
+        nod *q=p->next;
+        p->next=NULL;
+        delete(q);
+    }
+    else
+    {
+        nod *p=head;
+        head=NULL;
+        delete(p);
+    }
+    return head;
+}
+
+nod *stergereCheie(nod *head, int key) ///stergem primul nod cu val key
+{
+    if(head==NULL) return head;
+    if(head->key!=key)
+    {
+        nod *p=head;
+        while(p->next!=NULL && p->next->key!=key) p=p->next;
+        if(p->next!=NULL)
+        {
+            nod *q=p->next;
+            p->next=q->next;
+            delete(q);
+        }
+    }
+    else if(head->key==key)
+    {
+        nod *p=head;
+        head=head->next;
+        delete (p);
+    }
+    return head;
+}
+
 int main()
 {
-    int A[]={1,1,1,1,1,2,2,2,2,2};
+    int A[]={1,2,3,4,5,6,7,8,9};
     int n=sizeof(A)/sizeof(A[0]);
     for(int i=0;i<n;i++)
         head=inserareFinal(A[i],head);
         //head=inserareInceput(A[i],head);
-    head=inserareDupaOPoz(10,head,2);
-    head=inserareInainteDeOPoz(11,head,2);
+    //head=inserareDupaOPoz(10,head,2);
+    //head=inserareInainteDeOPoz(11,head,2);
+    head=stergereCheie(head,9);
     afisList(head);
     return 0;
 }
