aae8f8d1743c3b40883268ee2a1f3a6d446f5ce3
CalebSig <Sighiartau.Do.Achim@student.utcluj.ro>
2025-05-10 12:54:31 +0300

    Create BackTracking.cpp

---
+++ BackTracking.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int X[10],n;
+
+void afis(int k)
+{
+    for(int i=1;i<=k;i++)
+        cout<<X[i]<<" ";
+    cout<<'\n';
+}
+
+bool verif_sol(int k)
+{
+    return k==n;
+}
+
+bool verif(int k) ///verificare intermediara a solutiei
+{
+    for(int i=1;i<k;i++)
+        if(X[i]==X[k]) return 0;
+    return 1;
+}
+
+void back(int k) ///k = poz curenta pt care generam valoarea
+{
+    for(int i=1;i<=n;i++) ///generarea elementele din multimea M
+    {
+        X[k]=i;
+        if(verif(k)) ///se verifica intermediar solutia
+        {
+            if(verif_sol(k)) afis(k);
+            else back(k+1); ///generam pt poz urm
+        }
+    }
+}
+
+int main()
+{
+    cin>>n;
+    back(1); ///de la ce pozitie incepe generarea
+    return 0;
+}
